7df96e650ac2980cb9623fca90801312e0a2abf9
luorenjie <lrj193927@gmail.com>
2021-01-17 23:55:25 -0500

    add database connection

---
+++ src/main/java/org/northernjay/hospital_management_system/databaseConn.java
@@ -0,0 +1,22 @@
+package org.northernjay.hospital_management_system;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class databaseConn {
+
+    private static Connection con;
+    public static Connection getCon() throws ClassNotFoundException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("JDBC Driver is loaded successfully");
+
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3006/hospital_data", "root", "root");
+
+        } catch (Exception e) {
+            System.out.println("JDBC Driver not Found!");
+        }
+        return con;
+    }
+
+}
---
+++ src/main/resources/hospitalData-init.sql
@@ -0,0 +1,39 @@
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
+
+GRANT ALL PRIVILEGES ON * . * TO 'admin'@'localhost';
+
+CREATE DATABASE hospital_management;
+
+USE hospital_management;
+
+CREATE TABLE `admin` (
+    `id` INTEGER NOT NULL,
+    `password` VARCHAR(255) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+CREATE TABLE `patient`(
+    `id` INTEGER NOT NULL,
+    'firstname' VARCHAR(255) NOT NULL,
+    'lastname' VARCHAR(255) NOT NULL,
+    `inpatient` BOOLEAN NOT NULL,
+    PRIMARY KEY ('id')
+) ENGINE =InnoDB DEFAULT CHARSET = latin1;
+
+CREATE TABLE `lab_report`(
+    `id` INTEGER NOT NULL,
+    `patient_id` INTEGER NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    'date' TIMESTAMP NOT NULL,
+    PRIMARY KEY ('id'),
+    FOREIGN KEY ('patient_id') REFERENCES patient('id')
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+CREATE TABLE `billing` (
+    `id` INTEGER NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `patient_id` INTEGER NOT NULL,
+    `price` DECIMAL(15, 2) NOT NULL,
+    `date` TIMESTAMP NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+INSERT INTO `admin` VALUES ('admin', 'admin');
